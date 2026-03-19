f4788191725397c3e09eca7e446482a16be06254
Kostenko Yaroslav <kostenkoyaroskav1262000@gmail.com>
2026-03-19 19:40:40 +0200

    The laboratory work is completed

---
+++ react-intro/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <main className="main">
+      <h1>Ласкаво просимо</h1>
+      <p>Це моя перша лабораторна робота на React</p>
+      <button className="btn">Натисни мене</button>
+    </main>
+  );
+}
+
+export default Main;
